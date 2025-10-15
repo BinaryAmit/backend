703ce6e31ae1d9daef532848a618644fdff38cea
amit kumar <kumarroy9798@gmail.com>
2025-10-15 12:34:37 +0530

    updated

--- webrtc_meet/asgi.py
+++ webrtc_meet/asgi.py
@@ -1,12 +1,15 @@
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
-from signaling.routing import websocket_urlpatterns
 
+# Ensure settings are configured before importing anything that touches Django settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webrtc_meet.settings")
 
+from django.core.asgi import get_asgi_application
+
 django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from signaling.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
